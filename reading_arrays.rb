dc2a7a41e2ec33c0f3f21f5af8415207b07384b6
LyllianT <lilyterrific2008@gmail.com>
2018-07-11 16:12:43 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,20 +7,20 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
-    puts STUDENT_NAMES[0]
-end
-first_student_by_index
+# def first_student_by_index
+#     puts STUDENT_NAMES[0]
+# end
+# first_student_by_index
 
-def fourth_student_by_index
-  puts STUDENT_NAMES[4]
-end
-fourth_student_by_index
+# def fourth_student_by_index
+#   puts STUDENT_NAMES[4]
+# end
+# fourth_student_by_index
 
-def last_student_by_index
-  puts STUDENT_NAMES[5]
-end
-last_student_by_index
+# def last_student_by_index
+#   puts STUDENT_NAMES[5]
+# end
+# last_student_by_index
 
 def fourth_student_by_index
     STUDENT_NAMES[3]
