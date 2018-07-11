3c34ebb2e6ca1c13fa5dc7afed0d3ba3c8af0ca0
LyllianT <lilyterrific2008@gmail.com>
2018-07-11 16:03:03 +0000

    automatically backed up by learn

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -11,6 +11,8 @@ test_student_names = [
   "Kendrick Lamar",
 ]
 
+
+
 describe "first_student_by_index" do
   it "should return the correct String value" do
     expect(first_student_by_index).to eql("Adele"), "The first student, Adele, was not returned"
